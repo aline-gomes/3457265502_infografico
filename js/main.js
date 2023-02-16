f25498f727d82c8418f9d4cae0ea2e9492476ec3
Aline Gomes <aline-gomes@live.com>
2023-02-16 14:21:05 -0300

    Revert "commit infográfico drone original"
    This reverts commit e2037263b4f2876a8499fce127e7e564d73181c9.

--- js/main.js
+++ js/main.js
@@ -2,17 +2,15 @@
 gsap.to(drone, 4, { delay: 1, y: "+14%", ease: Linear.easeInOut, repeat: -1, yoyo: true });
 
 // POPS
-var pop_opened = [false, false, false, false, false, false];
 const pops = document.querySelector('.pop')
 const btns_pops = document.querySelectorAll('.btns')
 const pop_container = document.querySelector('.pop_container')
 
 function showPops(index) {
     pop_container.classList.remove('hide')
-    pops.innerHTML = txts_pops[index].title + txts_pops[index].image + txts_pops[index].txt + '<div class="line"></div>' + '<div class="divImg btn_close"></div>'
+    pops.innerHTML = txts_pops[index].title + txts_pops[index].txt + '<div class="line"></div>' + '<div class="divImg btn_close"></div>'
 
     btns_pops[index].style.cssText = 'filter: sepia(1);'
-    pop_opened[index] = true
     hidePops()
 }
 
--- js/txts_pops.js
+++ js/txts_pops.js
@@ -1,32 +1,26 @@
 const txts_pops = {
     0: {
         title: '',
-        image: '',
-        txt: '<p id="txt_01">O <strong>Drone Pro</strong> é um pulverizador de precisão.<br> Ele distribui agroquímicos ou sementes em quaisquer tipo de plantação, inclusive em locais de difícil acesso.</p>',
+        txt: '<p id="txt_01">Neque <strong>Lorem Ipsum</strong> sed imperdiet lobortis.<br> Suspendisse tempor, arcu et accumsan dapibus, magna purus congue augue, sed elementum risus magna vel ipsum.</p>'
     },
     1: {
-        title: '<h6>Usos:</h6>',
-        image: '',
-        txt: '<p id="txt_02"><span class="break_line"><strong>- Agricultura:</strong> pulverizações periódicas, aplicações de precisão, contenção de pragas e dispersão de sementes;</span><span class="break_line"><strong>- Pecuária:</strong> lançamento de sementes para plantio de pastagem (dispenser opcional);</span><span class="break_line"><strong>- Psicultura:</strong> dissipação de ração nos tanques.</span></p>'
+        title: '<h6>Lorem ipsum</h6>',
+        txt: '<p id="txt_02">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel euismod dolor, non convallis sem. Nulla a interdum eros, vestibulum ornare felis. Quisque ut velit quis eros accumsan fermentum. Integer semper mauris vitae dictum aliquam. Aenean vestibulum dapibus dui, sed consequat nunc lacinia sit amet.</span></p>'
     },
     2: {
-        title: '<h6>Benefícios:</h6>',
-        image: '',
-        txt: '<p id="txt_03"><span class="break_line"><strong>- Economia:</strong> Reduz o consumo de água, otimiza o uso de insumos e evita o pisoteio de cultivo;</span><span class="break_line"><strong>- Segurança:</strong> distância segura do operador e aplicação da quantidade precisa;</span><span class="break_line"><strong>- Eficiência:</strong> atua mesmo em áreas de difícil acesso;</span><span class="break_line"><strong>- Sustentabilidade:</strong> veículo elétrico, e com baixa dispersão de produto químico, reduzindo impactos no entorno;</span><span class="break_line"><strong>- Treinamento e suporte:</strong> treinamento in loco para operação e aplicação efetiva.</span></p>',
+        title: '<h6>Lorem ipsum</h6>',
+        txt: '<p id="txt_02">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel euismod dolor, non convallis sem. Nulla a interdum eros, vestibulum ornare felis. Quisque ut velit quis eros accumsan fermentum. Integer semper mauris vitae dictum aliquam. Aenean vestibulum dapibus dui, sed consequat nunc lacinia sit amet.</span></p>'
     },
     3: {
-        title: '<h6>Características:</h6>',
-        image: '',
-        txt: '<p id="txt_04"><span class="break_line">- Leve e compacto (até 25 KL);</span><span class="break_line">- Aplicação de insumos líquidos e sólidos;</span><span class="break_line">- Energia limpa, movido a bateria;</span><span class="break_line">- Duas opções de tamanho;</span><span class="break_line">- Visibilidade de navegação no controle remoto do Drone.</span></p>',
+        title: '<h6>Lorem ipsum</h6>',
+        txt: '<p id="txt_02">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel euismod dolor, non convallis sem. Nulla a interdum eros, vestibulum ornare felis. Quisque ut velit quis eros accumsan fermentum. Integer semper mauris vitae dictum aliquam. Aenean vestibulum dapibus dui, sed consequat nunc lacinia sit amet.</span></p>'
     },
     4: {
-        title: '<h6>Informações técnicas:</h6>',
-        image: '<div class="divImg pop_img_drone"></div>',
-        txt: '<p id="txt_05">Observação:<br><span class="break_line">- 6ha/h = 6 hectares por hora;</span><span class="break_line">- 16ha/h = 16 hectares por hora.</span></p>',
+        title: '<h6>Lorem ipsum</h6>',
+        txt: '<p id="txt_01">Neque <strong>Lorem Ipsum</strong> sed imperdiet lobortis.<br> Suspendisse tempor, arcu et accumsan dapibus, magna purus congue augue, sed elementum risus magna vel ipsum.</p>'
     },
     5: {
         title: '',
-        image: '',
-        txt: '<p id="txt_06">Além do <strong>Drone Pro,</strong> temos os drone de mapeamento agrícola, que serve para:<span class="break_line">- Monitoramento / Vigilância;</span><span class="break_line">- Mapeamento estratégico de grandes áreas;</span><span class="break_line">- Ferramenta de segurança para grandes áreas e com diversas aplicações.</span></p>',
+        txt: '<p id="txt_02">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel euismod dolor, non convallis sem. Nulla a interdum eros, vestibulum ornare felis. Quisque ut velit quis eros accumsan fermentum. Integer semper mauris vitae dictum aliquam. Aenean vestibulum dapibus dui, sed consequat nunc lacinia sit amet.</span></p>'
     }
 }
